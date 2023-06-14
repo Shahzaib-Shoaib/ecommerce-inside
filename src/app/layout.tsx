4c699b7cd3bed0008bff5283e9d20720e2ae8901
Shahzaib-Shoaib <82258192+Shahzaib-Shoaib@users.noreply.github.com>
2023-06-14 22:39:33 +0500

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,56 +15,56 @@ const poppins = Poppins({
   display: "swap",
 });
 
- export const metadata = {
-   title: {
-     default: siteConfig.name,
-     template: `%s · ${siteConfig.name}`,
-   },
-   description: siteConfig.description,
-   keywords: ["Wordpress", "Shopify", "Ecommerce"],
-  authors: [
-    {
-      name: "Ecommerce Inside",
-      url: siteConfig.social.github,
-    },
-  ],
-   creator: "Ecommerce Inside",
-   themeColor: [
-     { media: "(prefers-color-scheme: light)", color: "white" },
-     { media: "(prefers-color-scheme: dark)", color: "black" },
-   ],
+//  export const metadata = {
+//    title: {
+//      default: siteConfig.name,
+//      template: `%s · ${siteConfig.name}`,
+//    },
+//    description: siteConfig.description,
+//    keywords: ["Wordpress", "Shopify", "Ecommerce"],
+//   authors: [
+//     {
+//       name: "Ecommerce Inside",
+//       url: siteConfig.social.github,
+//     },
+//   ],
+//    creator: "Ecommerce Inside",
+//    themeColor: [
+//      { media: "(prefers-color-scheme: light)", color: "white" },
+//      { media: "(prefers-color-scheme: dark)", color: "black" },
+//    ],
 
-   openGraph: {
-    type: "website",
-    locale: "en_US",
-    url: siteConfig.siteUrl,
-    title: siteConfig.name,
-    description: siteConfig.description,
-    siteName: siteConfig.name,
-    images: [
-      {
-        url: `${siteConfig.siteUrl}/cover-pic.png`,
-        width: 1200,
-        height: 630,
-      },
-    ],
-  },
-  twitter: {
-    card: "summary_large_image",
-    title: siteConfig.name,
-    description: siteConfig.description,
-    creator: "Ecommerce Inside",
-  },
-  icons: {
-    icon: "/favicon.ico",
-    //shortcut: "/favicon-16x16.png",
-    //apple: "/apple-touch-icon.png",
-  },
-//   // other: {
-//   //   "google-site-verification": "SCtCAdftAAE0UptAZAoIYsHnG7xbuN_ofCaHgfDyjn4", // Google Search Console Verification
-//   // },
-manifest: "manifest.webmanifest",
- };
+//    openGraph: {
+//     type: "website",
+//     locale: "en_US",
+//     url: siteConfig.siteUrl,
+//     title: siteConfig.name,
+//     description: siteConfig.description,
+//     siteName: siteConfig.name,
+//     images: [
+//       {
+//         url: `${siteConfig.siteUrl}/cover-pic.png`,
+//         width: 1200,
+//         height: 630,
+//       },
+//     ],
+//   },
+//   twitter: {
+//     card: "summary_large_image",
+//     title: siteConfig.name,
+//     description: siteConfig.description,
+//     creator: "Ecommerce Inside",
+//   },
+//   icons: {
+//     icon: "/favicon.ico",
+//     //shortcut: "/favicon-16x16.png",
+//     //apple: "/apple-touch-icon.png",
+//   },
+// //   // other: {
+// //   //   "google-site-verification": "SCtCAdftAAE0UptAZAoIYsHnG7xbuN_ofCaHgfDyjn4", // Google Search Console Verification
+// //   // },
+// manifest: "manifest.webmanifest",
+//  };
 
 interface RootLayoutProps {
   children: ReactNode;
